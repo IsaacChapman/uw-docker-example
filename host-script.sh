5773fa04cd7b9995f8131051856f79f38b52d18e
Isaac Chapman <isaac@isaacchapman.com>
2016-07-01 14:40:14 -0700

    Trying with wrapdocker

--- host-script.sh
+++ host-script.sh
@@ -3,35 +3,42 @@
 set -e # Exit on error
 set -x # Echo on for logging
 
-# Ensure inner docker stops to prevent loopback device depletion
-function teardown {
-  set +e
-  kill -9 `cat /var/run/docker-in-docker.pid`
-  echo "### /var/log/docker.log ###"
-  cat /var/log/docker.log
-}
-trap teardown EXIT
+# wrapdocker variables
+DOCKER_DAEMON_ARGS="--storage-driver=vfs"
+LOG="file"
+DOCKER_GRAPH_PATH="/var/lib/docker/dind"
+mkdir -p $DOCKER_GRAPH_PATH
 
-# Start docker (vfs necessary)
-/solano/agent/docker daemon --storage-driver=vfs &>/var/log/docker.log &
+# Ensure inner docker stops to prevent loopback device depletion
+#function teardown {
+#  set +e
+#  kill -9 `cat /var/run/docker-in-docker.pid`
+#  echo "### /var/log/docker.log ###"
+#  cat /var/log/docker.log
+#}
+#trap teardown EXIT
+
+# Start docker
+/usr/local/repos/wrapdocker /solano/agent/docker
+#/solano/agent/docker daemon --storage-driver=vfs &>/var/log/docker.log &
 sleep 2
 
 # Pull docker image from registry
-sudo docker pull mysql:latest
+docker pull mysql:latest
 
 # Start docker container and capture its id
 CID=$(docker run -d -v /usr/local/repos/map_vol:/src -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} mysql:latest)
-DOCKER_PID=#!
+DOCKER_PID=$!
 echo $DOCKER_PID > /var/run/docker-in-docker.pid
 
 # Give mysql a couple of seconds to startup
 sleep 3
 
 # Show databases
-docker exec -it $CID bash -c mysql -u root -p${MYSQL_ROOT_PASSWORD} -e 'show databases'
+docker exec $CID bash -c mysql -u root -p${MYSQL_ROOT_PASSWORD} -e 'show databases'
 
 # Execute script on host
 /usr/local/repos/map_vol/hello.sh 'from host'
 
 # Execute script on docker container
-docker exec -it $CID bash -c /src/hello.sh 'from docker'
+docker exec $CID bash -c /src/hello.sh 'from docker'
