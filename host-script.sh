fc0789733d424a031d84dfa773c6e4bc2399cc19
Isaac Chapman <isaac@isaacchapman.com>
2016-06-30 21:58:51 -0700

    Add wrapdocker env vars

--- host-script.sh
+++ host-script.sh
@@ -11,6 +11,9 @@ ls -la
 ps afxu
 
 # Start docker
+export DOCKER_DAEMON_ARGS="--host=unix:///var/run/docker.sock --storage-driver=aufsext --dns=8.8.8.8 --icc=false"
+export DOCKER_GRAPH_PATH=/var/lib/docker-dind
+mkdir -p $DOCKER_GRAPH_PATH
 /usr/local/repos/wrapdocker /solano/agent/docker
 
 # Pull docker image from registry
