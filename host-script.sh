901f9c6bb4966c03592cdb9c478f8c5f40e6ebf2
Isaac Chapman <isaac@isaacchapman.com>
2016-06-30 20:13:46 -0700

    Use /usr/local/repos as repo root in scripts

---
+++ host-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e # Exit on error
+set -x # Echo on for logging
+
+# Pull docker image from registry
+docker pull mysql:latest
+
+# Start docker container and capture its id
+CID=$(docker run -d -v /usr/local/repos/map_vol:/src -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD mysql:latest)
+
+# Give mysql a couple of seconds to startup
+sleep 10
+
+# Show databases
+docker exec -it $CID bash -c mysql -u root -p${MYSQL_ROOT_PASSWORD} -e 'show databases'
+
+# Execute script on host
+/usr/local/repos/map_vol/hello.sh 'from host'
+
+# Execute script on docker container
+docker exec -it $CID bash -c /src/hello.sh 'from docker'
